9d7615a2fee979a758c4a479c633d5184b6c9f9d
francistalbot <francistalbot@hotmail.com>
2025-06-10 09:45:46 -0400

    Put Custom event Template in his own file

---
+++ resources/js/Components/Scheduler/EventTemplate.tsx
@@ -0,0 +1,16 @@
+import { Internationalization } from "@syncfusion/ej2-base";
+import { ScheduleDataItem } from "./ScheduleDataItem";
+
+const instance: Internationalization = new Internationalization();
+
+// Template personnalisée pour l'affichage des événements
+export const EventTemplate = (props: ScheduleDataItem) => (
+    <div className="e-appointment-details">
+        <div className="e-subject">{props.Subject}</div>
+        <div className="e-time">
+            {instance.formatDate(props.StartTime, { skeleton: "hm" })} -{" "}
+            {instance.formatDate(props.EndTime, { skeleton: "hm" })}
+        </div>
+        <div className="e-location">{props.Source}prout</div>
+    </div>
+);
