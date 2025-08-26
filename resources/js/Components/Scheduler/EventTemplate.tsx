789020d0ca64755503b094cfcc858190369db01d
francistalbot <francistalbot@hotmail.com>
2025-08-25 21:12:38 -0400

    modification of the event Templates

--- resources/js/Components/Scheduler/EventTemplate.tsx
+++ resources/js/Components/Scheduler/EventTemplate.tsx
@@ -1,16 +1,43 @@
 import { Internationalization } from "@syncfusion/ej2-base";
-import { Event } from "@/types/event";
 
 const instance: Internationalization = new Internationalization();
 
 // Template personnalisée pour l'affichage des événements
-export const EventTemplate = (props: Event) => (
-    <div className="e-appointment-details">
+export const EventTemplate = (props: any) => (
+    console.log("EventTemplate props:", props),
+    (
+        <div className="e-appointment-details">
+            <div className="e-inner-wrap">
+                <div className="e-subject">{props.Subject}</div>
+                <div className="e-time">
+                    {instance.formatDate(new Date(props.StartTime), {
+                        skeleton: "hm",
+                    })}{" "}
+                    -{" "}
+                    {instance.formatDate(new Date(props.EndTime), {
+                        skeleton: "hm",
+                    })}
+                </div>
+                <div className="e-location">{props.Location}</div>
+            </div>
+        </div>
+    )
+);
+
+// Template personnalisée pour l'affichage des événements dans l'agenda
+export const AgendaEventTemplate = (props: any) => (
+    <>
         <div className="e-subject">{props.Subject}</div>
-        <div className="e-time">
-            {instance.formatDate(new Date(props.StartTime), { skeleton: "hm" })}{" "}
-            - {instance.formatDate(new Date(props.EndTime), { skeleton: "hm" })}
+        <div className="e-date-time">
+            {instance.formatDate(new Date(props.StartTime), {
+                skeleton: "hm",
+            })}{" "}
+            -{" "}
+            {instance.formatDate(new Date(props.EndTime), {
+                skeleton: "hm",
+            })}
         </div>
         <div className="e-location">{props.Location}</div>
-    </div>
+        <div className="e-description">{props.Description}</div>
+    </>
 );
